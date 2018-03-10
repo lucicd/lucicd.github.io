e95c62c68d57cf0b01070007290985fc5f4e56c9
Drazen Lucic <lucicd@hotmail.com>
2018-03-10 18:13:49 +0400

    Assignment 9 first draft

--- assignments/lesson-9/scripts/index.js
+++ assignments/lesson-9/scripts/index.js
@@ -1,11 +1,12 @@
 (function() {
-  var section = document.querySelector('main section');
+  var section = document.querySelector('#about');
   var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
   var request = new XMLHttpRequest();
   request.open('GET', requestURL);
   request.responseType = 'json';
   request.onload = function() {
     var towns = request.response;
+    console.log(towns);
     showTowns(towns);
   }
   request.send();
@@ -16,33 +17,30 @@
     for (var i = 0; i < towns.length; i++) {
       if (towns[i].name === 'Placerton') continue;
       var myArticle = document.createElement('article');
+      var myDiv1 = document.createElement('div');
+      var myDiv2 = document.createElement('div');
       var myH2 = document.createElement('h2');
       var myPara1 = document.createElement('p');
       var myPara2 = document.createElement('p');
       var myPara3 = document.createElement('p');
       var myPara4 = document.createElement('p');
-      var myList = document.createElement('ul');
+      var myImg = document.createElement('img')
+      myImg.src = 'https://loremflickr.com/600/400/weather?lock=' + (i + 1) + '&random=' + towns[i].name;
 
       myH2.textContent = towns[i].name;
       myPara1.textContent = 'Moto: ' + towns[i].motto;
       myPara2.textContent = 'Year Founded: ' + towns[i].yearFounded;
       myPara3.textContent = 'Population: ' + towns[i].currentPopulation;
       myPara3.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
-      myPara4.textContent = 'Events:';
-          
-      var events = towns[i].events;
-      for (var j = 0; j < events.length; j++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = events[j];
-        myList.appendChild(listItem);
-      }
 
-      myArticle.appendChild(myH2);
-      myArticle.appendChild(myPara1);
-      myArticle.appendChild(myPara2);
-      myArticle.appendChild(myPara3);
-      myArticle.appendChild(myPara4);
-      myArticle.appendChild(myList);
+      myDiv1.appendChild(myH2);
+      myDiv1.appendChild(myPara1);
+      myDiv1.appendChild(myPara2);
+      myDiv1.appendChild(myPara3);
+      myDiv1.appendChild(myPara4);
+      myDiv2.appendChild(myImg)
+      myArticle.appendChild(myDiv1);
+      myArticle.appendChild(myDiv2);
 
       section.appendChild(myArticle);
     }
