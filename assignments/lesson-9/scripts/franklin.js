59a420e887c5300210bf862b87297eaa1f6c8357
Drazen Lucic <lucicd@hotmail.com>
2018-03-10 18:53:09 +0400

    Changed weather widget

--- assignments/lesson-9/scripts/franklin.js
+++ assignments/lesson-9/scripts/franklin.js
@@ -7,17 +7,15 @@
     var weatherInfo = JSON.parse(weatherObject.responseText);
     console.log(weatherInfo);
     var forecast = weatherInfo.forecast.simpleforecast;
-    document.getElementById('high-temp').innerHTML = forecast.forecastday[0].high.fahrenheit;
-    document.getElementById('low-temp').innerHTML = forecast.forecastday[0].low.fahrenheit;
+    var txtForecast = weatherInfo.forecast.txt_forecast;
+    document.getElementById('current-temp').innerHTML = forecast.forecastday[0].high.fahrenheit;
+    document.getElementById('forecast-desc').innerHTML = txtForecast.forecastday[0].fcttext;
     document.querySelector('.weather-icon').src = forecast.forecastday[0].icon_url;
     document.querySelector('.weather-desc').innerHTML = forecast.forecastday[0].conditions;
 
     var current = weatherInfo.current_observation
     document.querySelector('.main-title').innerHTML = current.display_location.full;
-    document.getElementById('precipitation').innerHTML = current.precip_today_in;
     document.getElementById('wind-speed').innerHTML = current.wind_mph;
-    document.getElementById('wind-direction').innerHTML = current.wind_dir;
-    document.getElementById('feels-like').innerHTML = current.windchill_f;
     for (var i = 0; i < 10; i++) {
       var selector = 'tbody tr td:nth-child(' + (i + 1) + ')';
       var highTemp = forecast.forecastday[i].high.fahrenheit;
