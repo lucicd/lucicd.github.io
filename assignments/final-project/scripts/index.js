908290723ac6df54a1c51be2358586d6e88f5510
Drazen Lucic <lucicd@hotmail.com>
2018-04-03 21:28:31 +0400

    Almost completed

--- assignments/final-project/scripts/index.js
+++ assignments/final-project/scripts/index.js
@@ -3,33 +3,27 @@ $(function() {
     $('#primaryNav').toggleClass('hide');
   });
 
-  var slideIndex = 1;
-  showSlides(slideIndex);
-
-  // Next/previous controls
   function plusSlides(n) {
     showSlides(slideIndex += n);
   }
 
-  // Thumbnail image controls
   function currentSlide(n) {
     showSlides(slideIndex = n);
   }
 
   function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
+    var slides = $('.mySlides');
+    var dots = $('.dot');
+    if (n > slides.length) {
+      slideIndex = 1;
+    } 
+    if (n < 1) {
+      slideIndex = slides.length;
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block"; 
-    dots[slideIndex-1].className += " active";
+    slides.css('display', 'none');
+    dots.removeClass('active');
+    slides.eq(slideIndex-1).css('display', 'block'); 
+    dots.eq(slideIndex-1).addClass('active');
   } 
   
   $('.prev').click(function() {
@@ -46,7 +40,42 @@ $(function() {
     });
   }
 
+  function currentDate() {
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] = 'Sunday';
+    weekday[1] = 'Monday';
+    weekday[2] = 'Tuesday';
+    weekday[3] = 'Wednesday';
+    weekday[4] = 'Thursday';
+    weekday[5] = 'Friday';
+    weekday[6] = 'Saturday';
+    
+    var months = new Array(12);
+    months[0] = 'January';
+    months[1] = 'February';
+    months[2] = 'March';
+    months[3] = 'April';
+    months[4] = 'May';
+    months[5] = 'June';
+    months[6] = 'July';
+    months[7] = 'August';
+    months[8] = 'September';
+    months[9] = 'October';
+    months[10] = 'November';
+    months[11] = 'December';
+    
+    var n = weekday[d.getDay()] + ',&nbsp;' +
+      d.getDate() + '&nbsp;' +
+      months[d.getMonth()] + '&nbsp;' + 
+      d.getFullYear();
+    $('#currentdate').html(n);
+  }
+
+  var slideIndex = 1;
+  showSlides(slideIndex);
   armDots(1);
   armDots(2);
   armDots(3);
+  currentDate();
 });
