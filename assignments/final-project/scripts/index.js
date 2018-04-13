f96e1af1d368c683120ecce98d05df71435cf34e
Drazen Lucic <lucicd@hotmail.com>
2018-04-13 15:29:13 +0400

    Home buttons work

--- assignments/final-project/scripts/index.js
+++ assignments/final-project/scripts/index.js
@@ -2,6 +2,17 @@ $(function() {
   $('#hamburgerBtn').click(function() {
     $('#primaryNav').toggleClass('hide');
   });
+
+  $('#joinEvent').click(function() {
+    window.location = 'events.html';
+  });
+  $('#shop').click(function() {
+    window.location = 'products.html';
+  });
+  $('#contactUs').click(function() {
+    window.location = 'contact.html';
+  });
+
   MyGallery();
   CurrentDate();
 });
