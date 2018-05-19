6bf128a08ccec64f5ca9f74bfe2a6e654d252b5e
Drazen Lucic <lucicd@hotmail.com>
2018-05-19 16:30:55 +0400

    Week4 completed

--- cit261/scripts/console.js
+++ cit261/scripts/console.js
@@ -1,4 +1,13 @@
 function show(message) {
-  var json = JSON.stringify(message);
-  document.getElementById('consoleOutput').innerHTML += json + '\n';
+  if (message === undefined) {
+    document.getElementById('consoleOutput').innerHTML += '\n';
+  } else {
+    var json;
+    if (typeof message === 'string') {
+      json = message;
+    } else {
+      json = JSON.stringify(message, undefined, 2);
+    }
+    document.getElementById('consoleOutput').innerHTML += json + '\n';    
+  }
 }
--- cit261/scripts/drazen-byui.js
+++
@@ -1,5 +0,0 @@
-(function(drazenByui, undefined) {
-  drazenByui.gebi = function(id) {
-    return document.getElementById(id);
-  };
-})(window.drazenByui = window.drazenByui || {});
